877fddbdd740f922674c10134de90075ed3ca85e
Julien <julien.thoumy@gmail.com>
2023-09-07 11:51:03 +0200

    plug-ing v1

--- Icon.js
+++ Icon.js
@@ -2,7 +2,7 @@ function getAllWeatherIcons() {
     const iconBasePath = "ressources/icon-meteo/png/";
 
     const weatherIcons = {
-        0: "unknown.png",
+        0: "unknown.svg",
         1000: "10000_clear_large.png",
         1100: "11000_mostly_clear_large.png",
         1101: "11010_partly_cloudy_large.png",
@@ -16,22 +16,22 @@ function getAllWeatherIcons() {
         2106: "21060_fog_mostly_clear_large.png",
         2107: "21070_fog_partly_cloudy_large.png",
         2108: "21080_fog_mostly_cloudy_large.png",
-        4000: "40000_drizzle.png",
-        4001: "40010_rain.png",
-        4200: "42000_light_rain.png",
-        4201: "42010_heavy_rain.png",
-        5000: "50000_snow.png",
-        5001: "50010_flurries.png",
-        5100: "51000_light_snow.png",
-        5101: "51010_heavy_snow.png",
-        6000: "60000_freezing_drizzle.png",
-        6001: "60010_freezing_rain.png",
-        6200: "62000_light_freezing_rain.png",
-        6201: "62010_heavy_freezing_rain.png",
-        7000: "70000_ice_pellets.png",
-        7101: "71010_heavy_ice_pellets.png",
-        7102: "71020_light_ice_pellets.png",
-        8000: "80000_thunderstorm.png"
+        4000: "40000_drizzle_large.png",
+        4001: "40010_rain_large.png",
+        4200: "42000_rain_light_large.png",
+        4201: "42010_rain_heavy_large.png",
+        5000: "50000_snow_large.png",
+        5001: "50010_flurries_large.png",
+        5100: "51000_snow_light_large.png",
+        5101: "51010_snow_heavy_large.png",
+        6000: "60000_freezing_rain_drizzle_large.png",
+        6001: "60010_freezing_rain_large.png",
+        6200: "62000_freezing_rain_light_large.png",
+        6201: "62010_freezing_rain_heavy_large.png",
+        7000: "70000_ice_pellets_large.png",
+        7101: "71010_ice_pellets_heavy_large.png",
+        7102: "71020_ice_pellets_light_large.png",
+        8000: "80000_tstorm_large.png"
     };
 
     const allIcons = {};
--- script.js
+++ script.js
@@ -1,28 +1,97 @@
+const options = {
+    method: 'GET',
+    headers: { accept: 'application/json' }
+};
 
+let cityName = 'Paris'; // Ville par défaut
 
-const options = { method: 'GET', headers: { accept: 'application/json' } };
+// Fonction pour mettre à jour les données météo
+function updateWeather() {
+    fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${cityName}&apikey=CqG3KRTU0z6LlN7su3zXtviZ2dVkzRNC`, options)
+        .then(response => response.json())
+        .then(data => {
+            const cloudCover = data.data.values.cloudCover;
+            const dewPoint = data.data.values.dewPoint;
+            const temperature = data.data.values.temperature;
+            const humidity = data.data.values.humidity;
+            const temperatureApparent = data.data.values.temperatureApparent;
+            const windDirection = data.data.values.windDirection;
+            const city = data.location.name.split(',')[0].trim();
+            const weatherCode = data.data.values.weatherCode;
 
-fetch('https://api.tomorrow.io/v4/weather/realtime?location=nuuk&apikey=CqG3KRTU0z6LlN7su3zXtviZ2dVkzRNC', options)
-    .then(response => response.json())
-    .then(data => {
-        const cloudCover = data.data.values.cloudCover;
-        const dewPoint = data.data.values.dewPoint;
-        const temperature = data.data.values.temperature;
-        const humidity = data.data.values.humidity;
-        const temperatureApparent = data.data.values.temperatureApparent;
-        const windDirection = data.data.values.windDirection;
-        const city = data.location.name.split(',')[0].trim();
-        //const weatherCode = data.data.values.weatherCode;
+            // Mettre à jour les éléments HTML avec les données
+            document.getElementById('cloudCover').textContent = cloudCover;
+            document.getElementById('dewPoint').textContent = dewPoint;
 
-        // Mettre à jour les éléments HTML avec les données
-        document.getElementById('cloudCover').textContent = cloudCover;
-        document.getElementById('dewPoint').textContent = dewPoint;
-        document.getElementById('temperature').textContent = `${temperature}°C`;
-        document.getElementById('humidity').textContent = `${humidity}%`;
-        document.getElementById('temperatureApparent').textContent = `${temperatureApparent}°C`;
-        document.getElementById('windDirection').textContent = windDirection;
-        document.getElementById('city').textContent = city;
-        
-        
-    })
-    .catch(err => console.error(err));
+            // Mettre à jour la classe CSS de la température en fonction de la valeur
+            const temperatureElement = document.getElementById('temperature');
+            temperatureElement.textContent = `${temperature}°C`;
+            updateTemperatureColor(temperatureElement, temperature);
+
+            document.getElementById('humidity').textContent = `${humidity}%`;
+            document.getElementById('temperatureApparent').textContent = `${temperatureApparent}°C`;
+            document.getElementById('windDirection').textContent = windDirection;
+            document.getElementById('city').textContent = city;
+
+            // Appeler la fonction pour gérer les icônes avec le nouveau code météo
+            handleWeatherIcons(weatherCode);
+        })
+        .catch(err => console.error(err));
+}
+
+// Fonction pour mettre à jour la couleur de la température en fonction de la valeur
+function updateTemperatureColor(element, temperature) {
+    if (temperature < 10) {
+        element.style.color = 'blue';
+    } else if (temperature > 25) {
+        element.style.color = 'orange';
+    } else if (temperature > 30) {
+        element.style.color = 'red';
+    } else {
+        // Réinitialiser la couleur par défaut
+        element.style.color = 'black';
+    }
+}
+
+// Fonction pour gérer les icônes en fonction du code météo
+function handleWeatherIcons(weatherCode) {
+    const allIcons = getAllWeatherIcons(); // Utilisez la fonction pour obtenir tous les chemins d'icônes
+
+    // Obtenez le chemin de l'icône en fonction du code météo
+    const iconPath = allIcons[weatherCode];
+
+    // Mettez à jour l'élément image avec l'icône correspondante
+    const iconImage = document.getElementById('weatherIcon');
+    iconImage.src = iconPath;
+}
+
+// Fonction pour obtenir tous les chemins d'icônes
+function getAllWeatherIcons() {
+    const iconBasePath = "ressources/icon-meteo/png/";
+
+    const weatherIcons = {
+        // Ajoutez ici votre mapping d'icônes
+    };
+
+    const allIcons = {};
+
+    for (const code in weatherIcons) {
+        if (weatherIcons.hasOwnProperty(code)) {
+            allIcons[code] = iconBasePath + weatherIcons[code];
+        }
+    }
+
+    return allIcons;
+}
+
+// Exécutez la mise à jour initiale des données météo
+updateWeather();
+
+// Ajoutez un gestionnaire d'événements pour le formulaire de recherche
+const searchForm = document.querySelector('form');
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const locationInput = document.getElementById('locationInput');
+    cityName = locationInput.value;
+    updateWeather();
+});
